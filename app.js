70d1f6ff7d7490b3688bcd686a70d040973ccc3f
Emanhusseinn <eman.hussein.jo@gmail.com>
2021-05-30 13:17:21 +0300

    $refs

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
-Vue.component('greeting', {
-    template: '<p>Hey there, I am a re-usable component</p>'
-});
-Vue.component('test',{
-    template: '<h1>FREE PALESTINE</h1>'
-})
-
-/* new Vue({
-    el: '.test',
-    template: '<p>I am a template</p>'
-}); */
-
 new Vue({
-    el: '#vue-app-one'
+    el: '#vue-app-one',
+    data: {
+        output: ''
+    },
+    methods: {
+        readRefs: function(){
+            console.log(this.$refs.input.value);
+            this.output = this.$refs.input.value;
+        }
+    }
 });
-
-new Vue({
-    el: '#vue-app-two'
-});
-
-new Vue({
-    el: '#vue-app-three'
-})
